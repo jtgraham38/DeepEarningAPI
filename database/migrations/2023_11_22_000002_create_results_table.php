575f68c99b842cb3faa7a0bf9f0049abfb53cfb9
Jacob <jtgraham38@gmail.com>
2023-11-22 12:39:16 -0500

    Added migrations for my api resources.

---
+++ database/migrations/2023_11_22_000002_create_results_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->integer('top');
+            $table->integer('left');
+            $table->integer('bottom');
+            $table->integer('right');
+            $table->string('angle')->nullable();
+            $table->string('distance')->nullable();
+            $table->string('subject')->nullable();
+            $table->string('clarity')->nullable();
+            $table->string('obscured')->nullable();
+            $table->string('orientation')->nullable();
+            $table->timestamps();
+            $table->unsignedBigInteger('image_id');
+            $table->foreign('image_id')
+                ->references('id')
+                ->on('images');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('results');
+    }
+};
