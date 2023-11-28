3948cafdc91a57b888f7d35291ca1334b80141c5
Jacob <jtgraham38@gmail.com>
2023-11-28 17:59:09 -0500

    Updated reference.

--- resources/views/endpoints.blade.php
+++ resources/views/endpoints.blade.php
@@ -7,66 +7,89 @@
     <title>Deep Earning API Reference</title>
 </head>
 <body>
-    <table>
-        <thead>
-           <tr>
-                <th>Method</th>
-                <th>Endpoint</th>
-                <th>Description</th>
-           </tr>
-        </thead>
-        <tbody>
+    <fieldset>
+        <legend>Deep Earning API Reference</legend>
+   
+        <table>
+            <thead>
             <tr>
-                <td>GET</td>
-                <td>/sets/</td>
-                <td>Returns a list of all image sets stored by the application.</td>
+                    <th>Method</th>
+                    <th>Endpoint</th>
+                    <th>Description</th>
             </tr>
-            <tr>
-                <td>GET</td>
-                <td>/sets/&lt;SET_ID&gt;/</td>
-                <td>Returns a listing for every image in a given data set.</td>
-            </tr>
-            <tr>
-                <td>GET</td>
-                <td>/sets/&lt;IMAGE_ID&gt;/</td>
-                <td>Gets details about a specific image from a data set.</td>
-            </tr>
-            <tr>
-                <td>GET</td>
-                <td>/results/&lt;IMAGE_ID&gt;/</td>
-                <td>Gets results about a specific image.</td>
-            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>GET</td>
+                    <td>/api/sets/&lt;SET_ID&gt;/</td>
+                    <td>Gets a set's data.</td>
+                </tr>
+                <tr>
+                    <td>POST</td>
+                    <td>/api/sets/</td>
+                    <td>Creates a new set.</td>
+                </tr>
+                <tr>
+                    <td>PATCH</td>
+                    <td>/api/sets/&lt;SET_ID&gt;/</td>
+                    <td>Updates a set.</td>
+                </tr>
+                <tr>
+                    <td>DELETE</td>
+                    <td>/api/sets/&lt;SET_ID&gt;/</td>
+                    <td>Deletes a set.</td>
+                </tr>
+                
+                <tr>
+                    <td>GET</td>
+                    <td>/api/images/&lt;IMAGE_ID&gt;/</td>
+                    <td>Gets an image's data.</td>
+                </tr>
+                <tr>
+                    <td>POST</td>
+                    <td>/api/images/</td>
+                    <td>Creates a new image.</td>
+                </tr>
+                <tr>
+                    <td>PATCH</td>
+                    <td>/api/images/&lt;IMAGE_ID&gt;/</td>
+                    <td>Updates an image.</td>
+                </tr>
+                <tr>
+                    <td>DELETE</td>
+                    <td>/api/images/&lt;IMAGE_ID&gt;/</td>
+                    <td>Deletes an image.</td>
+                </tr>
 
-            <tr>
-                <td>POST</td>
-                <td>/create_set/</td>
-                <td>Creates a new set to hold image records.</td>
-            </tr>
+                <tr>
+                    <td>GET</td>
+                    <td>/api/results/&lt;RESULT_ID&gt;/</td>
+                    <td>Gets a result's data</td>
+                </tr>
+                <tr>
+                    <td>POST</td>
+                    <td>/api/results/</td>
+                    <td>Creates a new result.</td>
+                </tr>
+                <tr>
+                    <td>PATCH</td>
+                    <td>/api/results/&lt;RESULT_ID&gt;/</td>
+                    <td>Updates a result.</td>
+                </tr>
+                <tr>
+                    <td>DELETE</td>
+                    <td>/api/results/&lt;RESULT_ID&gt;/</td>
+                    <td>Deletes a result.</td>
+                </tr>
 
-            <tr>
-                <td>POST</td>
-                <td>/add_to_set/&lt;SET_ID&gt;/</td>
-                <td>Adds a new image to an image set.</td>
-            </tr>
+            </tbody>
+        </table>
+    </fieldset>
 
-            <tr>
-                <td>POST</td>
-                <td>/update_image/&lt;IMAGE_ID&gt;/</td>
-                <td>Updates information about an image.</td>
-            </tr>
-
-            <tr>
-                <td>POST</td>
-                <td>/add_result/&lt;IMAGE_ID&gt;/</td>
-                <td>Adds a result for an image submitted by a user.</td>
-            </tr>
-
-            <tr>
-                <td>POST</td>
-                <td>/update_result/&lt;IMAGE_ID&gt;/</td>
-                <td>Updates a result for an image submitted by a user.</td>
-            </tr>
-        </tbody>
-    </table>
+    <style>
+        td{
+            padding: 2px;
+       }
+    </style>
 </body>
 </html>
