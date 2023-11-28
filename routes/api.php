f8e5fc1665288257c49dd54d0e9245c9495d9a5a
Jacob <jtgraham38@gmail.com>
2023-11-28 17:34:58 -0500

    Imports

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\SetController; 
+use App\Http\Controllers\Api\ImageController; 
+use App\Http\Controllers\Api\ResultController; 
 
 /*
 |--------------------------------------------------------------------------
